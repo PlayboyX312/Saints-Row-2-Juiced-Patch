6e1e8322beba9c351c3ee296cff44f017aadc2f6
Clippy95 <qpzezoqp15@gmail.com>
2025-06-06 23:38:13 +0300

    Skip aim assist function

--- Monkey Patch/Player/Input.cpp
+++ Monkey Patch/Player/Input.cpp
@@ -250,12 +250,12 @@ namespace Input {
 	typedef int __cdecl PlayerSpin(float a1);
 	PlayerSpin* UpdatePlayerSpin = (PlayerSpin*)(0x0073FB20); //0x00BD4A80
 	// If we need an empty global buffer we could use that, but it has to be 0.
-	volatile float aim_assist_empty_buffer[19]{};
+	//volatile float aim_assist_empty_buffer[19]{};
 	SafetyHookMid player_autoaim_do_assisted_aiming_midhook;
 	BYTE disable_aim_assist_noMatterInput;
 	SAFETYHOOK_NOINLINE void player_autoaim_do_assisted_aiming_midhookfunc_disableaimassistmouse(safetyhook::Context32& ctx) {
 		if((disable_aim_assist_noMatterInput >= 1 && g_lastInput == GAME_LAST_INPUT::MOUSE) || disable_aim_assist_noMatterInput >= 2)
-		ctx.esi = (uintptr_t)&aim_assist_empty_buffer;
+		ctx.eip = 0x9D7757;
 	}
 
 	volatile char KEY_inventory_up = 'W';
@@ -570,7 +570,7 @@ namespace Input {
 		}
 		LoadXInputDLL();
 
-			player_autoaim_do_assisted_aiming_midhook = safetyhook::create_mid(0x009E28B3, &player_autoaim_do_assisted_aiming_midhookfunc_disableaimassistmouse);
+			player_autoaim_do_assisted_aiming_midhook = safetyhook::create_mid(0x009D7752, &player_autoaim_do_assisted_aiming_midhookfunc_disableaimassistmouse);
 			Logger::TypedLog(CHN_MOD, "Disabling Aim Assist while using mouse...\n");
 			disable_aim_assist_noMatterInput = (BYTE)std::clamp((int)GameConfig::GetValue("Gameplay", "DisableAimAssist", 1), 0, 2);
 		
