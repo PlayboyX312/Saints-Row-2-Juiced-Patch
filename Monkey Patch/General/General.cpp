f3e46e0b38b0388a90db9648687aea6dccd50b17
Tervel1337 <78919226+Tervel1337@users.noreply.github.com>
2025-06-26 23:19:43 +0300

    Fix placeholder string resolving (e.g. OUTFIT_NAME_) for vint to point to the right offset

--- Monkey Patch/General/General.cpp
+++ Monkey Patch/General/General.cpp
@@ -1143,6 +1143,9 @@ void __declspec(naked) TextureCrashFixRemasteredByGroveStreetGames()
 
 	}
 	void TopWinMain() {
+		static auto PlaceholderStringFix = safetyhook::create_mid(0x00B92C0F, [](SafetyHookContext& ctx) {
+			ctx.ebp = ctx.eax + 2; // eax + 2 because the game returns a bad pointer that always starts with 01 00 instead of the string start
+		});
 		Logger::TypedLog("D3D9", "D3D9 Hook: %d", D3D9Hook::initialize());
 		allowJuicedAPI = GameConfig::GetValue("API", "JuicedAPI", 1);
 		bitmap_loader::Init();
